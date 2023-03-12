740ca01a9270912e6315a117399fada70eff0f44
Pritam Dey <pritamdey2001@gmail.com>
2023-03-13 01:27:32 +0530

    urlencoded

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ dotenv.config({ path: './config/config.env' });
 const app = express();
 
 app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({ extended: true }));
 
 
 app.use('/auth', userRouter);
