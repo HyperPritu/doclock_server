c98a9a9e1149d3afa0e56b5a7a0c223120445e58
Pritam Dey <pritamdey2001@gmail.com>
2023-03-13 00:37:28 +0530

    changed upload documents

--- routes/documents.js
+++ routes/documents.js
@@ -8,10 +8,10 @@ const router = express.Router();
 
 router.get('/', auth, async (req, res) => {
 	const documents = await DocumentModel.find({ user: req.user.id });
-	res.status(200).json({ success: true, totalDocuments: documents.length });
+	res.status(200).json({ success: true, totalDocuments: documents.length, documents });
 });
 
-router.post('/', auth, multer({ storage: multer.memoryStorage() }).single('file'), async (req, res) => {
+router.post('/', auth, multer({ storage: multer.memoryStorage(), limits: { fileSize: 10 * 1024 * 1024 } }).single('file'), async (req, res) => {
 	try {
 		const filename = req.body.filename;
 		const user = req.user.id;
