2a698ab06f36e6f9fe143caaf09d064e3d940b7f
Pritam Dey <pritamdey2001@gmail.com>
2023-03-15 00:21:51 +0530

    bug fixing

--- routes/documents.js
+++ routes/documents.js
@@ -13,7 +13,7 @@ router.get('/', auth, async (req, res) => {
 
 router.post('/', auth, multer({ storage: multer.memoryStorage() }).single('file'), async (req, res) => {
 	try {
-		console.log(req.file);
+		console.log("User: " + req.user + ", Body: " + req.body);
 		const filename = req.body.filename;
 		const user = req.user.id;
 
