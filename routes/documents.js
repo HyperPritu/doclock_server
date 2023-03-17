ad869d8a31f21b343079d78979c0fffa1fb68ab6
HyperPritu <pritamdey2001@gmail.com>
2023-03-17 12:14:50 +0530

    added delete functionality

--- routes/documents.js
+++ routes/documents.js
@@ -1,34 +1,46 @@
-import express from 'express';
-import { DocumentModel } from '../models/Document.js';
-import auth from '../middleware/user_jwt.js';
-import multer from 'multer';
-import { fileUpload } from '../config/cloudinary.js';
+import express from "express";
+import { DocumentModel } from "../models/Document.js";
+import auth from "../middleware/user_jwt.js";
+import multer from "multer";
+import { fileUpload } from "../config/cloudinary.js";
 
 const router = express.Router();
 
-router.get('/', auth, async (req, res) => {
-	const documents = await DocumentModel.find({ user: req.user.id });
-	res.status(200).json({ success: true, totalDocuments: documents.length, documents });
+router.get("/", auth, async (req, res) => {
+  const documents = await DocumentModel.find({ user: req.user.id });
+  res.status(200).json({ success: true, totalDocuments: documents.length, documents });
 });
 
-router.post('/', auth, multer({ storage: multer.memoryStorage() }).single('file'), async (req, res) => {
-	try {
-		const filename = req.body.filename;
-		const user = req.user.id;
+router.post("/", auth, multer({ storage: multer.memoryStorage() }).single("file"), async (req, res) => {
+  try {
+    const filename = req.body.filename;
+    const user = req.user.id;
 
-		const isFileExist = await DocumentModel.findOne({ filename, user });
+    const isFileExist = await DocumentModel.findOne({ filename, user });
 
-		if (isFileExist) return res.status(409).json({ success: false, msg: 'Document already exists!' });
+    if (isFileExist) return res.status(409).json({ success: false, msg: "Document already exists!" });
 
-		const result = await fileUpload(req.file, 'documents');
+    const result = await fileUpload(req.file, "documents");
 
-		const newDocument = new DocumentModel({ public_id: result.public_id, filename, file_url: result.secure_url, user });
-		await newDocument.save();
+    const newDocument = new DocumentModel({ public_id: result.public_id, filename, file_url: result.secure_url, user });
+    await newDocument.save();
 
-		res.status(200).json({ success: true, msg: 'Uploaded Successfully', public_id: result.public_id, filename: filename, file_url: result.url, user: user });
-	} catch (error) {
-		console.warn(error);
-	}
+    res.status(200).json({ success: true, msg: "Uploaded Successfully", public_id: result.public_id, filename: filename, file_url: result.url, user: user });
+  } catch (error) {
+    console.warn(error);
+  }
+});
+
+router.delete("/:public_id", async (req, res) => {
+  try {
+    const public_id = req.params.public_id;
+    cloudinary.uploader.destroy(public_id, function (error) {
+      if (error) throw error;
+      else return res.status(200).json({ success: true, msg: "Deleted Succesfully" });
+    });
+  } catch (e) {
+    console.warn(e);
+  }
 });
 
 export { router as documentRouter };
